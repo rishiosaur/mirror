8ff0e57b96c4e372310cc2ce72b5a1a0779e4b7d
Rishi Kothari <hey@rishi.cx>
2022-04-03 23:57:19 -0400

    add current route

---
+++ pages/api/spotify/current.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { baseSpotifyApi, spotify } from '../../../src/util/token'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const response = await spotify(
+		'https://api.spotify.com/v1/me/player/currently-playing'
+	)
+
+		const json = await response.json()
+	console.log(json)
+
+	res.json(json)
+}
+
+export default handler
